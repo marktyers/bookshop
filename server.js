cb136309621513f905867ce576116409bc09834d
Mark Tyers <marktyers@gmail.com>
2014-04-13 13:26:12 +0100

    Removed inline book POST resource.

--- server.js
+++ server.js
@@ -11,17 +11,5 @@
     server.put('/user/:username', user.addUser)
     server.get('/user/:username', user.getUser)
 
-    server.post('/book/:isbn', function(req, res) {
-        var auth = req.authorization
-        console.log(auth)
-        var user = req.authorization.username
-        var passwd = req.authorization.password
-        var isbn = parseInt(req.params.isbn)
-        var genre = parseInt(req.params.genre)
-        var response = {authorization:auth, isbn:isbn, genre:genre}
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify(response.headers))
-    })
-
     server.listen(8080)
     console.log('listening on port 8080')
